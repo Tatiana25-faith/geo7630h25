23771f3756d23ae4e61b3c4c11988db8e3e6d4ee
Tatiana25-faith <goncalves.tatiana_patricia@courrier.uqam.ca>
2025-04-26 23:45:34 +0000

    labos

--- Laboratoire12/app/randomPoints.js
+++ Laboratoire12/app/randomPoints.js
@@ -1,12 +1,12 @@
 const randomPoints = {
   type: "FeatureCollection",
-  features: Array.from({ length: 50 }, (_, i) => ({
+  features: Array.from({ length: 2000 }, (_, i) => ({
     type: "Feature",
     geometry: {
       type: "Point",
       coordinates: [
-        -73.55 + (Math.random() - 0.5) * 0.1,
-        45.55 + (Math.random() - 0.5) * 0.1
+        -74.1 + Math.random() * 1.2,  // Longitudes entre -74.1 et -72.9
+        45.3 + Math.random() * 0.8    // Latitudes entre 45.3 et 46.1
       ]
     },
     properties: {
